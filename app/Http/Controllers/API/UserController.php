6680f3bcb860f76578a4d00e24652b7df8ca3c48
tws9797 <twenseng7227@gmail.com>
2020-01-20 19:27:35 +0800

    Sorting

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
     public function index()
     {
       if (\Gate::allows('isAdmin') || \Gate::allows('isAuthor')) {
-          return User::latest()->paginate(5);
+          return User::orderBy('id', 'asc')->paginate(5);
       }
     }
 
